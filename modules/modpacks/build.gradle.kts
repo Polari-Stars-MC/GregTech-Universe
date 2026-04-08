87b3220acae45f82c41fdb58b518718ee9eee572
baka4n <qq474899581@gmail.com>
2026-04-08 19:37:08 +0800

    修复jei版本问题

--- modules/modpacks/build.gradle.kts
+++ modules/modpacks/build.gradle.kts
@@ -1,6 +1,10 @@
 dependencies {
     implementation("com.simibubi.create:create-1.21.1:6.0.10-272:slim") {
         exclude("info.journeymap")
+        exclude("mezz.jei")
     }
     implementation("com.gregtechceu.gtceu:gtceu-1.21.1:7.5.2-SNAPSHOT")
+    compileOnly("mezz.jei:jei-1.21.1-neoforge:19.27.0.340")
+    localRuntime("mezz.jei:jei-1.21.1-neoforge:19.27.0.340")
+
 }
