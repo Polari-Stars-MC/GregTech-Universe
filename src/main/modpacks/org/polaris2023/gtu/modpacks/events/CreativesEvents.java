7c6d4824e8db1365a147540c33f7f6ef5e1f037c
baka4n <qq474899581@gmail.com>
2026-04-12 00:50:33 +0800

    修改完毕

--- src/main/modpacks/org/polaris2023/gtu/modpacks/events/CreativesEvents.java
+++ src/main/modpacks/org/polaris2023/gtu/modpacks/events/CreativesEvents.java
@@ -11,42 +11,16 @@ import net.neoforged.neoforge.common.CreativeModeTabRegistry;
 import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
 import org.polaris2023.gtu.core.init.CreativeTabRegistries;
 import org.polaris2023.gtu.modpacks.GregtechUniverseModPacks;
+import org.polaris2023.gtu.modpacks.init.ItemRegistries;
 
 @EventBusSubscriber(modid = GregtechUniverseModPacks.MOD_ID)
 public class CreativesEvents {
     @SubscribeEvent
     public static void event(BuildCreativeModeTabContentsEvent event) {
         if (event.getTabKey().equals(CreativeTabRegistries.MAIN.getKey())) {
-            {
-                ItemStack stack = BuiltInRegistries.ITEM.get(ResourceLocation.fromNamespaceAndPath("gtceu", "flint_axe")).getDefaultInstance();
-                stack.set(DataComponents.MAX_DAMAGE, (int) (stack.getMaxDamage() * 1.2F));
-                stack.set(DataComponents.CUSTOM_NAME, Component.translatable("item.gtu_modpacks.flint_rope_axe"));
-                event.accept(stack);
-            }
-            {
-                ItemStack stack = BuiltInRegistries.ITEM.get(ResourceLocation.fromNamespaceAndPath("gtceu", "flint_pickaxe")).getDefaultInstance();
-                stack.set(DataComponents.MAX_DAMAGE, (int) (stack.getMaxDamage() * 1.2F));
-                stack.set(DataComponents.CUSTOM_NAME, Component.translatable("item.gtu_modpacks.flint_rope_pickaxe"));
-                event.accept(stack);
-            }
-            {
-                ItemStack stack = BuiltInRegistries.ITEM.get(ResourceLocation.fromNamespaceAndPath("gtceu", "flint_hoe")).getDefaultInstance();
-                stack.set(DataComponents.DAMAGE, (int) (stack.getMaxDamage() * 1.2F));
-                stack.set(DataComponents.CUSTOM_NAME, Component.translatable("item.gtu_modpacks.flint_rope_hoe"));
-                event.accept(stack);
-            }
-            {
-                ItemStack stack = BuiltInRegistries.ITEM.get(ResourceLocation.fromNamespaceAndPath("gtceu", "flint_sword")).getDefaultInstance();
-                stack.set(DataComponents.DAMAGE, (int) (stack.getMaxDamage() * 1.2F));
-                stack.set(DataComponents.CUSTOM_NAME, Component.translatable("item.gtu_modpacks.flint_rope_sword"));
-                event.accept(stack);
-            }
-            {
-                ItemStack stack = BuiltInRegistries.ITEM.get(ResourceLocation.fromNamespaceAndPath("gtceu", "flint_shovel")).getDefaultInstance();
-                stack.set(DataComponents.DAMAGE, (int) (stack.getMaxDamage() * 1.2F));
-                stack.set(DataComponents.CUSTOM_NAME, Component.translatable("item.gtu_modpacks.flint_rope_shovel"));
-                event.accept(stack);
-            }
+            ItemRegistries.REGISTER.getEntries().forEach(h -> {
+                event.accept(h.get());
+            });
 
         }
     }
